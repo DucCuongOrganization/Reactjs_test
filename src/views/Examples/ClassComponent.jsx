a92c27b0021e908a765d5ad79b4b40b0997fa6eb
DucCuong159 <cuong.pd192732@gmail.com>
2024-08-21 00:41:36 +0700

    LESSON-5 State

--- src/views/Examples/ClassComponent.jsx
+++ src/views/Examples/ClassComponent.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 class ClassComponent extends React.Component {
+  state = {
+    name: "Cuong",
+    age: 23,
+  };
+
+  handleEventChange(e) {
+    return this.setState({ ...this.state, name: e.target.value });
+  }
+
   render() {
-    return <h1>Hello, Class Component!</h1>;
+    return (
+      <>
+        <input type="text" onChange={(e) => this.handleEventChange(e)} value={this.state.name}/>
+        <h1>Hello, {this.state.name} {this.state.age}!</h1>
+      </>
+    );
   }
 }
 
