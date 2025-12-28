62a95a97a95ef0c4c83773d6fa2e63ccfeae4078
DucCuong159 <cuong.pd192732@gmail.com>
2025-12-28 14:35:36 +0700

    feat: configure dayjs Vietnamese locale globally
    - Add dayjs locale import and configuration in index.tsx
    - Ensures consistent Vietnamese localization across the app
    - Future-proof for displaying Vietnamese month/day names

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import dayjs from "dayjs";
+import "dayjs/locale/vi";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./store/index";
 import "./styles/global.scss";
 import App from "./views/App";
+
+// Configure dayjs locale globally
+dayjs.locale("vi");
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
